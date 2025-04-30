814f8f139b613b0231fff3c06db4ef1b221c1f6e
lauraduranrmz <durananalaura130@gmail.com>
2025-04-29 20:53:57 -0600

    sesión 29 de abril

--- Ejemplo02_Sesión3/Persona.java
+++ Ejemplo02_Sesión3/Persona.java
@@ -1,5 +1,5 @@
 package Ejemplo02_Sesión3;
 
 public class Persona {
-    //hacer este ejemplo02, hacer ejemplo03 y el reto01
+    //hacer este ejemplo02, hacer ejemplo03 y el reto01 y el reto02 sesion3
 }
